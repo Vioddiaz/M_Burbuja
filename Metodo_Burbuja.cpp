22a2cee2c5efe4a125f45f99467f048b8acd5811
Vioddiaz <160296626+Vioddiaz@users.noreply.github.com>
2024-02-16 22:43:11 -0600

    Add files via upload

---
+++ Metodo_Burbuja.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+    int arreglo[] = {24, 2, 19, 26, 9, 4, 20, 11, 5, 22};
+    int i, j, aux;
+    int comparaciones = 0;
+    int intercambios = 0;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8 - i - 1; j++) {
+            comparaciones++;
+            if (arreglo[j] > arreglo[j + 1]) {
+                intercambios++;
+                aux = arreglo[j];
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = aux;
+            }
+        }
+    }
+
+    cout << "Arreglo en orden Ascendente:";
+    for (i = 0; i < 8; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << "\nArreglo en orden Descendente: ";
+    for(i=7; i>=0; i--){
+        cout << arreglo[i] << " ";
+    }
+
+    cout << "\nEl numero de comparaciones que tuvo el arreglo es de: " << comparaciones << endl;
+    cout << "El numero de intercambios que tuvo el arreglo es de: " << intercambios << endl;
+
+    getch();
+}
